8e6cbf4ed9e14af90eb717b0d5c0bab6d071371a
Ztuarthz <micheel.pvargas@gmail.com>
2023-04-28 12:27:20 -0500

    Actualizacion de datos

--- js/script.js
+++ js/script.js
@@ -1,23 +1,58 @@
-function encriptar(){
-    let texto = document.getElementById("txtArea");
-    let tituloMensaje = document.getElementById("tituloMensaje");
-    let parrafo = document.getElementById("mensajeParrafo");
-    let imagen = document.getElementById("imagen");
+//Obtencion de variables, acepto y encriptacion por replace
+const texto = document.getElementById('txtArea');
+const imagen = document.getElementById('imagen');
+const resultado = document.getElementById('resultado');
+const copiar = document.getElementById('copiar');
+const error = document.getElementById('error');
+const acentos = /[ÁÉÍÓÚÜÑáéíóúüñ]/;
+const encriptacion = texto => {
+    return texto
+    .replace(/a/g, "ai")
+    .replace(/e/g, "enter")
+    .replace(/i/g, "imes")
+    .replace(/o/g, "ober")
+    .replace(/u/g, "ufat")
+}
+const desencriptacion = texto => {
+    return texto
+    .replace(/enter/g, "e")
+    .replace(/imes/g, "i")
+    .replace(/ai/g, "a")
+    .replace(/ober/g, "o")
+    .replace(/ufat/g, "u")
+}
 
-    let textoEncriptado = texto
-        .replace(/e/gi, "enter")
-        .replace(/o/gi, "ober")
-        .replace(/i/gi, "imes")
-        .replace(/a/gi, "ai")
-        .replace(/u/gi, "ufat");
-
-    if (texto.length !=0){
-        texto = textoEncriptado;
-        tituloMensaje.textContent = "Texto Encriptado con éxito"
-        parrafo.textContent = "";
-        
-    }else{
-        alert("Ingrese un mensaje a encriptar")
-        }   
+//Funcion encriptar
+function encriptar() {
+    textoFinal = '';
+    let textoInicial = texto.value.toLowerCase();
+    let acento = verAcentos( textoInicial );
+    if ( textoInicial.trim() == '' ) {
+        window.location.reload();
     }
-
+    if ( textoInicial != ''  &&  acento != true ) {
+        textoFinal = encriptacion( textoInicial );
+        imagen.classList.add( "ocultarImagen" );
+        resultado.textContent = textoFinal;
+        copiar.removeAttribute('hidden');
+    }    
+}
+//Funcion desencriptar
+function desencriptar() {
+    textoFinal = '';
+    let textoInicial = texto.value.toLowerCase();
+    let acento = verAcentos( textoInicial );
+    if ( textoInicial.trim() == '' ) {
+        window.location.reload();
+    }
+    if ( textoInicial != ''  &&  acento != true ) {
+        textoFinal = desencriptacion( textoInicial );
+        imagen.classList.add( "ocultarImagen" );
+        resultado.textContent = textoFinal;
+        copiar.removeAttribute('hidden');
+    }    
+}
+//boton copiar
+copiar.addEventListener('click', ()=>{
+    navigator.clipboard.writeText( textoFinal );
+})
